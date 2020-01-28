9d27a6f2c121f12709b7e3c55afd916ecde445ea
Misha <visvan3333@gmail.com>
2020-01-28 14:18:32 +0300

    task1

---
+++ homework/homeworksGit/main.js
@@ -0,0 +1,13 @@
+var a, b, c;
+a =	Math.round(Math.random()*10);
+b = a * 3;
+c = a + b;
+alert("a = " + a);
+alert("b = " + b);
+alert("c = " + c);
+
+// <<<<<<<<<<<<task2>>>>>>>>>>
+var  firstName, lastName;
+firstName = prompt("your firstname","");
+lastName = prompt("your lastname","");
+alert("What’s up" + firstName.UpperCase() + lastName.UpperCase());
